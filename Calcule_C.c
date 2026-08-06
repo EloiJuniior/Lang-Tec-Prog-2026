36efce891449e7726d1fd646e09e340a79ac7c49
Eloi Junior <junixxz@hotmail.com>
2026-08-05 22:28:54 -0300

    Add files via upload

---
+++ Calcule_C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+ 
+int main(int argc, char *argv[]) {
+	
+	int a,b,c;
+	float resultado; 
+	
+	a = 100;
+	b = 150;
+	c = 200;
+	resultado = (a+b+c)/3;
+	printf("resultado %f", resultado);
+	
+	return 0;
+}
