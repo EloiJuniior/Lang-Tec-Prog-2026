a1681526678b187400731356ea78b7f667eb1f74
Eloi Junior <junixxz@hotmail.com>
2026-09-02 22:26:56 -0300

    Add files via upload
    Aula do dia 02/09/2026 Quarta-feira

--- main.c
+++ main.c
@@ -4,36 +4,32 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int resposta;
+	printf("Usuario, qual exercicio quer resolver? |2|3|8|\n");
+	scanf("%d", &resposta);
 	
-	int numero;
+	switch (resposta)
+	case 2:
 	
-	printf("Digite um numero inteiro: ");
-	scanf("d% , &numero");
+	;
+	//Faça um programa que leia um valor em reais e catação do dolar. Em seguida, imprima o valor correspondente em dólar
+	float dola, real, cotaa;
+	scanf("%f", &real);
+	scanf("%f", &cota);
+	printf("%f reais são %f dolinhos", real, (real/cota));
 	
-	if (numero > 0){
-		printf("O inverso do numero e: %.2f\n", 1.0 / numero);
-	} else {
-		printf("O Quadrado de numero e: %d\n", numero* numero);
-	}
+	//Leia um valor que represente uma temperatura em graus Celsius e apresente-a convertida em graus fahrenheit.
+	float tempC, tempF;
+	scanf("%f", &tempC);
+	tempF = tempC *(9.0/5.0) + 32.0;
+	printf("A temperatura maxima de hoje na tela da globo %f", tempF);
 	
-/*	
-	printf("Insira os valores de A, B, C: ");
-	scanf("%d %d %d", &a, &b, &c);
-	
-	if (a>b){
-		resultado = a;
-	}else{
-		resultado = b;
-	}
-	
-	if (c>resultado){
-		resultado = c;
-	}
-	
-	printf("%d e o maior", resultado);
-	
-	*/
-	
-	
+	//(URI 1019) Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e
+	int segundos;
+	scanf("%f", &segundos);
+	horas = segundos/3600 ;
+	minutos = (segundos-(horas*3600))60;
+	segundos = segundos-((horas*3600)+(minutos*60));
+	printf("Tempo %d:%d%d", (segundos%36000))
 	return 0;
 }
